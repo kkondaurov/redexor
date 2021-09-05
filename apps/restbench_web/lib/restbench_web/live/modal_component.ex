5ba6ec02ed07b918b551df0e0a99cdcf848809e0
Konstantin Kondaurov <konstantin@kondaurov.com>
2021-09-05 18:45:47 +0400

    Add styling to Mock Server CRUD forms

--- apps/restbench_web/lib/restbench_web/live/modal_component.ex
+++ apps/restbench_web/lib/restbench_web/live/modal_component.ex
@@ -6,15 +6,19 @@ defmodule RestbenchWeb.ModalComponent do
     ~H"""
     <div
       id={@id}
-      class="phx-modal"
+      class="phx-modal card mb-4"
       phx-capture-click="close"
       phx-window-keydown="close"
       phx-key="escape"
       phx-target={@myself}
       phx-page-loading>
 
-      <div class="phx-modal-content">
-        <%= live_patch "&times;", to: @return_to, class: "phx-modal-close" %>
+      <div class="phx-modal-content card-content">
+        <%= live_patch to: @return_to, class: "button is-small is-danger has-icons-left has-text-light" do %>
+        <span class="icon is-small">
+          <i class="fas fa-times"></i>
+        </span>
+        <% end %>
         <%= live_component @component, @opts %>
       </div>
     </div>
