4e9e3721d9da4350a3507a87a0d105287f58c9b2
Konstantin Kondaurov <konstantin@kondaurov.com>
2021-09-15 22:51:43 +0400

    Clean up home page

--- apps/restbench_web/test/restbench_web/controllers/page_controller_test.exs
+++ apps/restbench_web/test/restbench_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule RestbenchWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "RestBench"
+    assert html_response(conn, 200) =~ "restbench"
   end
 end
