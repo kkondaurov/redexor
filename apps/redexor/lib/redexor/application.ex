3a3689173e8bed36b212289f5986b9b467c26ec0
Konstantin Kondaurov <konstantin@kondaurov.com>
2021-09-20 14:18:11 +0400

    Only start RequestLogger in non-test environments

--- apps/redexor/lib/redexor/application.ex
+++ apps/redexor/lib/redexor/application.ex
@@ -11,11 +11,13 @@ defmodule Redexor.Application do
       Redexor.Repo,
       # Start the PubSub system
       {Phoenix.PubSub, name: Redexor.PubSub},
-      Redexor.RequestLogger
       # Start a worker by calling: Redexor.Worker.start_link(arg)
       # {Redexor.Worker, arg}
-    ]
+    ] ++ children_for_env(Mix.env())
 
     Supervisor.start_link(children, strategy: :one_for_one, name: Redexor.Supervisor)
   end
+
+  def children_for_env(:test), do: []
+  def children_for_env(_), do: [Redexor.RequestLogger]
 end
