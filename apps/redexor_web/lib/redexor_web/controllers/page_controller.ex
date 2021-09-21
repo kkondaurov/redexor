62c4a580025c8ef87168d3690beb205afe4d5376
Konstantin Kondaurov <konstantin@kondaurov.com>
2021-09-21 14:55:57 +0400

    Add screenshot gallery to the home page

--- apps/redexor_web/lib/redexor_web/controllers/page_controller.ex
+++ apps/redexor_web/lib/redexor_web/controllers/page_controller.ex
@@ -2,9 +2,6 @@ defmodule RedexorWeb.PageController do
   use RedexorWeb, :controller
 
   def index(conn, _params) do
-    case conn.assigns.current_user do
-      nil -> render(conn, "index.html")
-      _user -> render(conn, "dashboard.html")
-    end
+    render(conn, "index.html")
   end
 end
