b5ba9a85383352e5774af8806104d7a75428e70b
Konstantin Kondaurov <konstantin@kondaurov.com>
2021-11-21 11:29:52 +0400

    Stop the service before running migrations in deployment scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,5 +3,4 @@ MIX_ENV=prod mix deps.get --only prod \
 && MIX_ENV=prod mix compile \
 && MIX_ENV=prod mix assets.deploy --force \
 && MIX_ENV=prod mix release --overwrite \
-&& MIX_ENV=prod mix phx.digest.clean --all \
-&& _build/prod/rel/redexor/bin/redexor eval "Redexor.Release.seed"
+&& MIX_ENV=prod mix phx.digest.clean --all
