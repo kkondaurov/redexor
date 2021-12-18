11962bad9348d2dcf6ab78ae7181489902f0a33d
Konstantin Kondaurov <konstantin@kondaurov.com>
2021-12-18 10:54:52 +0400

    Basic setup for fly.io deployment

--- config/runtime.exs
+++ config/runtime.exs
@@ -13,7 +13,8 @@ if config_env() == :prod do
       """
 
   config :redexor, Redexor.Repo,
-    # ssl: true,
+    ssl: false,
+    socket_options: [:inet6],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\redexor" eval Redexor.Release.migrate
